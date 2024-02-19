c47830849f883c4e7e7a46b1bd51e9ee9a8fd912
Simon <Simon-Dirks@users.noreply.github.com>
2024-02-19 17:08:14 +0100

    Update tree_nav_helper.rb

--- app/helpers/tree_nav_helper.rb
+++ app/helpers/tree_nav_helper.rb
@@ -3,6 +3,7 @@ module TreeNavHelper
     box_name_str = box_name.to_s
     logger.info(box_filename_prefixes_data.key?(box_name_str))
     logger.info(box_name_str)
+    logger.info(box_filename_prefixes_data[box_name_str])
     return false unless box_filename_prefixes_data.key?(box_name_str)
     box_filename_prefixes_data[box_name_str].any? { |filename| filename.start_with?(prefix) }
   end

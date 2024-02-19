ef23e76e0293c7958dcd1a46967502c03419da9d
Simon <Simon-Dirks@users.noreply.github.com>
2024-02-19 15:07:02 +0100

    Update tree_nav_helper.rb

--- app/helpers/tree_nav_helper.rb
+++ app/helpers/tree_nav_helper.rb
@@ -1,5 +1,6 @@
 module TreeNavHelper
   def box_contains_page_title?(nav_data, box_name, page_title)
     nav_data[box_name].values.flatten.any? { |filenames| filenames.include?(page_title) }
+    logger.info("Page title #{page_title} is#{contains_page_title ? '' : ' not'} contained in box #{box_name} #{nav_data[box_name].values.flatten}")
   end
 end

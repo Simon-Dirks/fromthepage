c3e785b63ae3268f1c8177b5c77c3a840ff32a9f
Simon <Simon-Dirks@users.noreply.github.com>
2024-02-19 17:07:21 +0100

    Pass box key as str

--- app/helpers/tree_nav_helper.rb
+++ app/helpers/tree_nav_helper.rb
@@ -1,9 +1,9 @@
 module TreeNavHelper
-  def box_contains_filename_prefix?(box_filename_prefixes_data, box_number, prefix)
-    box_number_str = box_number.to_s
-    logger.info(box_filename_prefixes_data.key?(box_number_str))
-    logger.info(box_number_str)
-    return false unless box_filename_prefixes_data.key?(box_number_str)
-    box_filename_prefixes_data[box_number_str].any? { |filename| filename.start_with?(prefix) }
+  def box_contains_filename_prefix?(box_filename_prefixes_data, box_name, prefix)
+    box_name_str = box_number.to_s
+    logger.info(box_filename_prefixes_data.key?(box_name_str))
+    logger.info(box_name_str)
+    return false unless box_filename_prefixes_data.key?(box_name_str)
+    box_filename_prefixes_data[box_name_str].any? { |filename| filename.start_with?(prefix) }
   end
 end

abd5a3258d8aef420b7e4b7c36419b85e8ace1de
Simon <Simon-Dirks@users.noreply.github.com>
2024-02-19 17:04:38 +0100

    Add helper

--- app/helpers/tree_nav_helper.rb
+++ app/helpers/tree_nav_helper.rb
@@ -1,3 +1,7 @@
 module TreeNavHelper
- 
+  def box_contains_filename_prefix?(box_filename_prefixes_data, box_number, prefix)
+    box_number_str = box_number.to_s
+    return false unless box_filename_prefixes_data.key?(box_number_str)
+    box_filename_prefixes_data[box_number_str].any? { |filename| filename.start_with?(prefix) }
+  end
 end

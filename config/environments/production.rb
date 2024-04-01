ae89cb93f3cef169f17c931a09dbc2e07b73e161
Simon <Simon-Dirks@users.noreply.github.com>
2024-04-01 15:21:33 +0200

    Update production.rb (based on develop env)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,109 +1,73 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  # Code is not reloaded between requests.
   config.cache_classes = true
-
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
-  # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
-  config.action_controller.perform_caching = true
+  config.consider_all_requests_local = true
+  # config.action_controller.perform_caching = true
 
-  # Enable Rack::Cache to put a simple HTTP cache in front of your application
-  # Add `rack-cache` to your Gemfile before enabling this.
-  # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
-  # config.action_dispatch.rack_cache = true
+  # Enable/disable caching. By default caching is disabled.
+  # Run rails dev:cache to toggle caching.
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
+    config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+    }
+  else
+    config.action_controller.perform_caching = false
 
-  # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :terser
-  config.assets.css_compressor = :sass
+    config.cache_store = :null_store
+  end
 
-  # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.active_storage.service = :local
 
-  # Generate digests for assets URLs.
-  config.assets.digest = true
+  config.action_mailer.raise_delivery_errors = false
 
-  # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.action_mailer.perform_caching = false
 
-  # Specifies the header that your server uses for sending files.
-  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  Rails.application.routes.default_url_options = config.action_mailer.default_url_options =  { host: 'localhost:3000' }
 
-  # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  config.force_ssl = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
 
-  # BWB 2018-08-16 changed from :info to :warn
-  # BWB 2018-11-17 changed from :warn to :info
-  # Set to :debug to see everything in the log.
-  #config.log_level = :info
-  config.log_level = :debug
+  config.active_support.deprecation = :log
 
-  # Prepend all log lines with the following tags.
-  # config.log_tags = [ :subdomain, :uuid ]
+  config.active_record.migration_error = :page_load
 
-  # Use a different logger for distributed setups.
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.active_record.verbose_query_logs = true
 
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
+  # number of complex assets.
+  config.assets.debug = true
 
-  # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.action_controller.asset_host = "http://assets.example.com"
+  config.assets.quiet = true
 
-  # Precompile additional assets.
-  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  # Raises error for missing translations.
+  # config.action_view.raise_on_missing_translations = true
 
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  # Use an evented file watcher to asynchronously detect changes in source code,
+  # routes, locales, etc. This feature depends on the listen gem.
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
-  # the I18n.default_locale when a translation cannot be found).
-  config.i18n.fallbacks = true
+  Rails.application.routes.default_url_options[:host] = 'localhost:3000'
 
-  # Send deprecation notices to registered listeners.
-  config.active_support.deprecation = :notify
-
-  # Disable automatic flushing of the log to improve performance.
-  # config.autoflush_log = false
-
-  # Use default logging formatter so that PID and timestamp are not suppressed.
-  config.log_formatter = ::Logger::Formatter.new
-
-  # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-Rails.application.routes.default_url_options[:host] = 'fromthepage.com'
-  config.action_mailer.default_url_options = { host: 'fromthepage.com' }
-  config.action_mailer.default_options = { from: 'support@fromthepage.com' }
-  config.action_mailer.smtp_settings = {
-    :address   => "smtp.mandrillapp.com",
-    :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
-    :enable_starttls_auto => true, # detects and uses STARTTLS
-    :user_name => "FromThePage",
-    :password  => ENV['MANDRILL_API_KEY'], # SMTP password is any valid API key
-    :authentication => 'login', # Mandrill supports 'plain' or 'login'
-    :domain => 'fromthepage.com', # your domain to identify your server when connecting
-  }
+  config.i18n.fallbacks = true
+  config.force_ssl = true
+    
+  NEATO = '/usr/bin/neato'
+  RAKE = '/usr/bin/env rake'
+  TEX_PATH='/usr/local/texlive/2017/bin/x86_64-linux/'
+
+  config.pontiiif_server = 'http://pontiiif.brumfieldlabs.com/'
+
+  ## Config for MailCatcher ##
+  # Install mailcatcher locally on your machine 'gem install mailcatcher'
+  # Run 'mailcatcher' in the terminal to start the server
+  # Open 'http://localhost:1080/' in your browser to see mail sent
 
   Ahoy.geocode = false
-  config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
-
-  RAKE = '/usr/bin/env rake'
-  NEATO = '/usr/bin/neato'
-  TEX_PATH='/usr/local/texlive/2017/bin/x86_64-linux/'
-#  BILLING_HOST_DEVELOPMENT = 'billing.fromthepage.com'
-#  Disable IIIF search while Pontiiif is down
-#  config.pontiiif_server = 'http://pontiiif.brumfieldlabs.com/'
-
 end

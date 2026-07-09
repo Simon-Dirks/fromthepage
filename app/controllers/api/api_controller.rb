a0276bac86786197b6bb0d08c07f8c26e8181cef
Simon Dirks <mail@simondirks.com>
2026-07-09 12:25:29 +0200

    Require authentication in ApiController
    Prevents /api or /api.json from being publicly accessible

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,5 +1,7 @@
 module Api
   class ApiController < ApplicationController
+    before_action :authenticate_user!
+
     # real code should exist inside the versioned controllers -- this is just a helper guide
     def help
       api_routes = Rails.application.routes.routes.select { |r| r.path.spec.to_s.starts_with? "/api" }
